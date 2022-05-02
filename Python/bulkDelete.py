6533d8e78bf971470987131045025d8cdd807448
Sourcery AI <>
2022-05-02 08:49:25 +0000

    'Refactored by Sourcery'

--- Python/bulkDelete.py
+++ Python/bulkDelete.py
@@ -11,7 +11,7 @@ begin = datetime.date(2019,6,1)
 end = datetime.date(2019,6,7)
 
 for table in tables:
-        print("{}.{}.{}".format(table.project, table.dataset_id, table.table_id))
+        print(f"{table.project}.{table.dataset_id}.{table.table_id}")
         for i in range((end - begin).days+1):
             day = begin + datetime.timedelta(days=i)
             os.system("bq rm -f -t 'DATASET.TABLENAME$day'") 
